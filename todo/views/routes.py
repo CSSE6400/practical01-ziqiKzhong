f69f3b64f04d99dcab4e661586672e0b714aa999
ziqiKzhong <95021268+ziqiKzhong@users.noreply.github.com>
2023-02-22 02:29:43 +0000

    curl & rest

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
-@api.route('/health')
+#@api.route('/health')
+@api.route('/health', methods=['POST'])
 def health():
+
     return jsonify({"status": "ok"})
